aafffe7c6d82ed56d3a1297a85e3f73bb0b1dcbc
Nikki Ma <m.nikki@wustl.edu>
2026-03-30 13:18:58 -0500

    remove duplicate imports and render App correctly

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-console.log("running")
-
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 
 const root = document.getElementById('root')
 
-createRoot(root).render(<App />)
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
