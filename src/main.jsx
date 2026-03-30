a439ac7ceac98919353b8a04e5665d2f743b4e25
Nikki Ma <m.nikki@wustl.edu>
2026-03-30 12:21:56 -0500

    Added square component and render it inside App

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
+console.log("running")
+
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+const root = document.getElementById('root')
+
+createRoot(root).render(<App />)
