e0966351774e6512e12b0a42e624fabb111049bc
Nikki Ma <m.nikki@wustl.edu>
2026-03-30 14:41:38 -0500

    attempting to render

--- my-tictactoe/src/App.jsx
+++ my-tictactoe/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './styles.css'
+import '.my-tictactoe/src/styles.css'
 
 export default function App() {
   return (
